7ac84dbb2758a3e2526027b8ccca7abcebaf65ec
Foddie2 <Bensonagala@live.com>
2022-07-16 17:54:47 +0300

    Update page responsivenes

--- pages/Update.vue
+++ pages/Update.vue
@@ -1,32 +1,29 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div>
-     <section class="vh-100" style="background-color: #FCFAFA;">
+  <section class="vh-100" style="background-color: #FCFAFA;">
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col col-xl-6">
         <div class="card" style="border-radius: 1rem;">
           <div class="row g-0">
-            <div class="col-md-6 col-lg-7 d-flex align-items-center">
               <div class="card-body p-4 p-lg-5 text-black">
                   <div class="pt-1 mb-1">
                     <button class="btn btn-dark btn-lg btn-block" type="button">Update</button>
                   </div>
-                    <div class="pt-1 mb-1">
+                    <div class="pt-4 mb-1">
                     <button class="btn btn-dark btn-lg btn-block" type="button">Receive</button>
                   </div>
-                    <div class="pt-1 mb-1">
+                    <div class="pt-4 mb-1">
                     <button class="btn btn-dark btn-lg btn-block" type="button">Outbound</button>
                   </div>
-                    <div class="pt-1 mb-1">
+                    <div class="pt-4 mb-1">
                     <button class="btn btn-dark btn-lg btn-block" type="button">Reports</button>
                   </div>
-
               </div>
             </div>
           </div>
         </div>
-      </div>
     </div>
   </div>
 </section>
