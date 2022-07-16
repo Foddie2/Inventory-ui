d880589bcf38d7ffc1cc225972bf309312f4587b
Foddie2 <Bensonagala@live.com>
2022-07-16 13:16:00 +0300

    login page setup

---
+++ components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header class="header">
+      <h1 class="title">Benson</h1>
+      <ul>
+        <li>
+
+        </li>
+      </ul>
+    </header>
+
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+
+</style>
---
+++ pages/Home.vue
@@ -0,0 +1,31 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <h1>Inventory management</h1>
+    <p>Hello welcome to inventory management system</p>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    head() {
+      return {
+        title: 'Home',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'Manage your inventory with us '
+
+          }
+        ]
+      };
+    }
+
+  };
+</script>
+
+<style>
+
+</style>
