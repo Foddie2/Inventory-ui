e5451d0bb2545bcf7f7b12b311e72a76e24aed1c
Foddie2 <Bensonagala@live.com>
2022-07-18 12:28:22 +0300

    Changes to the Appheader

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -8,14 +8,17 @@
 
     <div class="d-flex input-group w-auto">
 
-      <button type="button" class="btn btn-primary me-3 d-flex ">
-          Refresh
-        </button>
-         <button type="button" class="btn btn-primary me-3 d-flex">
-          Logout
-        </button>
-      <span class="input-group-text border-0" id="search-addon">
-        <i class="fas fa-search"></i>
+      <button type="button" class="btn btn-primary me-3 d-flex  mr-4">
+        Menu
+      </button>
+      <button type="button" class="btn btn-primary me-3 d-flex  mr-4">
+        Home
+      </button>
+      <button type="button" class="btn btn-primary me-3 d-flex  mr-4">
+        Logout
+      </button>
+      <span class="input-group-text border-0">
+        <i class="bi bi-person"></i>
       </span>
     </div>
   </div>
